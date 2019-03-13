aadfc341d2a86ddc637b371e6cb9d286537cafa7
sharmila kandagirivasan <sharmila.k@wearefmg.net>
2019-03-13 19:20:14 +0530

    Adding header

--- application/views/header.php
+++ application/views/header.php
@@ -33,8 +33,8 @@
 	<!--15--><script src="<?php echo base_url();?>/public/js/sidebar.js"></script>
 	
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>	-->
   
 	
 
